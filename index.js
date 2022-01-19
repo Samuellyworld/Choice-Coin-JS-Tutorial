9b7307ef2827ca394a189638765448c39efd5e5d
Aspirin <Aspirin@Samuel-Aspirins-Mac.local>
2022-01-19 15:43:17 +0100

    wait for confirmation functionalities to sync 🥑

--- index.js
+++ index.js
@@ -89,8 +89,23 @@ const chooseVotingOption = async () => {
 
 chooseVotingOption();
 
-// check result
+//verification function
+const waitForConfirmation = async function (algodClient, txId) {
+    let lastround = (await algodClient.status().do())['last-round'];
+     while (true) {
+        const pendingInfo = await algodClient.pendingTransactionInformation(txId).do();
+        if (pendingInfo['confirmed-round'] !== null && pendingInfo['confirmed-round'] > 0) {
+          //Got the completed Transaction
+          console.log('Transaction confirmed in round ' + pendingInfo['confirmed-round']);
+          break;
+        }
+        lastround++;
+        await algodClient.statusAfterBlock(lastround).do();
+     }
+ };
+ 
 
+// check account balance
 const checkResult = async () => {
     
     
